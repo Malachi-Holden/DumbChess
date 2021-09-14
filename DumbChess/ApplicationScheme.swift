8bbe2b7767010c373f844fd67ec08eae12c0071f
Malachi Holden <malachi@suran.com>
2021-09-14 11:01:27 -0400

    setup material components

---
+++ DumbChess/ApplicationScheme.swift
@@ -0,0 +1,44 @@
+//
+//  ApplicationScheme.swift
+//  DumbChess
+//
+//  Created by Malachi Holden on 9/13/21.
+//
+
+import Foundation
+import MaterialComponents
+
+class ApplicationScheme: NSObject {
+    let containerScheme = { () -> MDCContainerScheming in
+        let scheme = MDCContainerScheme()
+        scheme.colorScheme.primaryColor =
+          UIColor(red: 0.0/255.0, green: 92.0/255.0, blue: 178.0/255.0, alpha: 1.0)
+        scheme.colorScheme.primaryColorVariant =
+          UIColor(red: 106.0/255.0, green: 183.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        scheme.colorScheme.onPrimaryColor =
+            .white
+        scheme.colorScheme.secondaryColor =
+            UIColor(red: 66.0/255.0, green: 66.0/255.0, blue: 66.0/255.0, alpha: 1.0)
+        scheme.colorScheme.onSecondaryColor =
+          UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        scheme.colorScheme.surfaceColor =
+            .white
+        scheme.colorScheme.onSurfaceColor =
+            UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)
+        scheme.colorScheme.backgroundColor =
+            .white
+        scheme.colorScheme.onBackgroundColor =
+            .black
+        scheme.colorScheme.errorColor =
+            .red
+        scheme.typographyScheme.button = UIFont.preferredFont(forTextStyle: .largeTitle)
+        return scheme
+    }()
+    
+
+  private static var singleton = ApplicationScheme()
+
+  static var shared: ApplicationScheme {
+    return singleton
+  }
+}
--- DumbChess/UI/SetupViewController.swift
+++ DumbChess/UI/SetupViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MaterialComponents
 
 protocol SetupViewControllerDelegate {
     func onChoosePlayerColor(_ color: PieceColor)
@@ -14,9 +15,18 @@ protocol SetupViewControllerDelegate {
 class SetupViewController: UIViewController {
     
     var delegate: SetupViewControllerDelegate?
-
+    @IBOutlet weak var playAsWhiteButton: MDCButton!
+    @IBOutlet weak var playAsBlackButton: MDCButton!
+    @IBOutlet weak var playRandomButton: MDCButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scheme = ApplicationScheme.shared.containerScheme
+        self.view.backgroundColor = scheme.colorScheme.surfaceColor
+        self.view.tintColor = scheme.colorScheme.onSurfaceColor
+        playAsWhiteButton.applyTextTheme(withScheme: scheme)
+        playAsBlackButton.applyTextTheme(withScheme: scheme)
+        playRandomButton.applyTextTheme(withScheme: scheme)
     }
     
 
